a1a67e08c364c93cb6d4c23a4a9522bdd9efc834
sugengprianto <55155451+sugengprianto@users.noreply.github.com>
2023-08-09 14:14:48 +0700

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,7 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
+    siteTitle: `Blog`,
     siteTitleAlt: `Minimal Blog - Gatsby Theme`,
     siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
     siteUrl: `https://minimal-blog.lekoarts.de`,
@@ -39,7 +39,7 @@ const config: GatsbyConfig = {
             url: `https://twitter.com/lekoarts_de`,
           },
           {
-            name: `Homepage`,
+            name: `Beranda`,
             url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
           },
         ],
